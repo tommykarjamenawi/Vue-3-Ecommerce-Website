f343d5fb837b379c219bffd61967cf8f98f75a5d
tommykarjamenawi <tommy_12345@live.nl>
2023-03-16 14:45:04 +0100

    added sweetalert2

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -66,9 +66,12 @@
       Download PDF
     </button>
   </div>
+
+  <button @click="showAlert">Show alert!</button>
 </template>
 
 <script>
+import Swal from "sweetalert2";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 
@@ -115,6 +118,27 @@ export default {
           canvas.height
         );
         pdf.save("example.pdf");
+      });
+    },
+    showAlert() {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      }).then((result) => {
+        if (result.value) {
+          Swal.fire(
+            "Deleted!",
+            "Your imaginary file has been deleted.",
+            "success"
+          );
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire("Cancelled", "Your imaginary file is safe :)", "error");
+        }
       });
     },
   },
